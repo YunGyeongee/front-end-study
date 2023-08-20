bfc186dda0106bd22aff9662faca5b0c65522278
rachel <yuuung26@gmail.com>
2023-08-20 10:41:08 +0900

    set to-do list

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,28 +1,29 @@
 import { useState, useEffect } from "react";
 
 function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
-  console.log('all time');
-
-  useEffect(() => {
-    // state가 아무리 변해도 코드를 딱 한 번만 실행하는 함수.
-    console.log('only one time');
-  }, []);
-
-  useEffect(() => {
-    if (keyword !== "" & keyword.length > 5) {
-      console.log('Search Movie : ', keyword);
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([])
+  const onchange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
     }
-  }, [keyword]);
-
+    setToDos((currentArray) => [toDo, ...currentArray])
+    setToDo("");
+  }
   return (
     <div>
-      <input value={keyword} onChange={onChange} type="text" placeholder="Search here..." />
-      <h1>{ counter }</h1>
-      <button onClick={onClick}>click me</button>
+      <h1>My To Do List (total : { toDos.length })</h1>
+      <form onSubmit={ onSubmit }>
+        <input onChange={ onchange } value={ toDo } type="text" placeholder="Write your to do..."/>
+        <button>Add To Do</button>
+      </form>
+      <hr/>
+      {/* .map의 기능 : 기존 배열에 있는 값들을 원하는 대로 변형할 수 있음 */ }
+      <ul>
+        { toDos.map((item, index) => <li key={index}>{ item }</li>) }
+      </ul>
     </div>
   );
 }
